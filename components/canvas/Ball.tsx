b05b24f0b46b9fd8445c27f1fc69bf242a163087
Ramadhoni <ramadhonimuhammad612@gmail.com>
2024-04-27 16:40:30 +0800

    -

--- components/canvas/Ball.tsx
+++ components/canvas/Ball.tsx
@@ -5,13 +5,13 @@ import {
   Float,
   OrbitControls,
   Preload,
-  useTexture,
+  // useTexture,
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Ball = ({ imgUrl }: any) => {
-  const decal = useTexture(imgUrl);
+  // const decal = useTexture(imgUrl);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
@@ -29,7 +29,7 @@ const Ball = ({ imgUrl }: any) => {
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
-          map={decal}
+          // map={decal}
         />
       </mesh>
     </Float>
